38a72bcf4118dc4d9049d8a98afd61e0c2b7074b
Scot Murphy <scot@scoizzle.com>
2014-05-31 22:37:54 -0500

    Initial Commit!
    I like freedom, irc, and explosions.

---
+++ Bot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Poly;
+using Poly.Data;
+using Poly.Net.Irc;
+
+namespace Poly.Bot {
+    public partial class Bot : Client {
+        public Bot() {
+        }
+        public void On(string Name, Event.Handler Func) {
+            this.AddEvent("On" + Name, Event.Wrapper(Func, "this", this));
+        }
+        
+        public void Configure(jsObject Args) {
+            Args.CopyTo(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.IO;
+
+using System.Diagnostics;
+
+using Poly.Data;
+using Poly.Net.Irc;
+using Poly.Script;
+
+
+namespace Poly.Bot {
+    public class Program : App {
+        static void Main(string[] args) {
+            var Engine = new Script.Engine();
+            App.Init(Log.Levels.Info);
+
+            if (File.Exists("Config.psx")) {
+                if (Engine.Parse(File.ReadAllText("Config.psx"))) {
+                    Engine.Evaluate(new jsObject());
+                }
+            }
+
+            while (App.Running) {
+                Console.ReadLine();
+            }
+        }
+    }
+}
