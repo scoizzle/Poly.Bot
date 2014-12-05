9a54ad4548335359ebd0c07d6f0b61514fce1985
Scot Murphy <scot@scoizzle.com>
2014-12-04 23:47:25 -0600

    Functionality improvements and updated code for PolyScript 2.0

--- Bot.cs
+++ Bot.cs
@@ -11,6 +11,7 @@ namespace Poly.Bot {
     public partial class Bot : Client {
         public Bot() {
         }
+
         public void On(string Name, Event.Handler Func) {
             this.AddEvent("On" + Name, Event.Wrapper(Func, "this", this));
         }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks; 
+using System.Threading; 
 using System.IO;
 
 using System.Diagnostics;
@@ -16,16 +16,18 @@ namespace Poly.Bot {
     public class Program : App {
         static void Main(string[] args) {
             App.Init(Log.Levels.Info, args);
+            jsObject Context = new jsObject();
+
             var Engine = new Script.Engine();
 
             if (File.Exists("Config.psx")) {
                 if (Engine.Parse(File.ReadAllText("Config.psx"))) {
-                    Engine.Evaluate(new jsObject());
+                    Engine.Evaluate(Context);
                 }
             }
 
-
-            while (App.Running) { }
-        }
+            while (App.Running) 
+                Thread.Sleep(150);
+		}
     }
 }   
