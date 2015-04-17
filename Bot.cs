419c5cd6539e00b19a2ae32622cf5f08cac8df78
Scot Murphy <scot@scoizzle.com>
2015-04-16 21:03:06 -0500

    Boom.

--- Bot.cs
+++ Bot.cs
@@ -7,13 +7,22 @@ using Poly;
 using Poly.Data;
 using Poly.Net.Irc;
 
-namespace Poly.Bot {
+namespace Poly {
     public partial class Bot : Client {
         public Bot() {
         }
 
         public void On(string Name, Event.Handler Func) {
-            this.AddEvent("On" + Name, Event.Wrapper(Func, "this", this));
+            this.AddEvent("On" + Name, Event.Wrapper(Func, "Bot", this));
+        }
+
+
+        public void On(string Name, Event.Handler Func, object This) {
+            this.AddEvent("On" + Name, Event.Wrapper(Func, "Bot", this, "this", This));
+        }
+
+        public void On(string Name, Event.Handler Func, string ThisName, object This) {
+            this.AddEvent("On" + Name, Event.Wrapper(Func, "Bot", this, ThisName, This));
         }
         
         public void Configure(jsObject Args) {
--- Program.cs
+++ Program.cs
@@ -4,28 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading; 
 using System.IO;
-
 using System.Diagnostics;
 
 using Poly.Data;
 using Poly.Net.Irc;
 using Poly.Script;
 
+using Poly.Script.Nodes;
+using Poly.Script.Types;
 
-namespace Poly.Bot {
+using MySql.Data.MySqlClient;
+
+namespace Poly {
     public class Program : App {
         static void Main(string[] args) {
             App.Init(Log.Levels.Info, args);
-            jsObject Context = new jsObject();
 
             var Engine = new Script.Engine();
-
+            var Context = new jsObject();
+            
             if (File.Exists("Config.psx")) {
                 if (Engine.Parse(File.ReadAllText("Config.psx"))) {
                     Engine.Evaluate(Context);
                 }
             }
 
+                        
             while (App.Running) 
                 Thread.Sleep(150);
 		}
